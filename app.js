caf7fb8547f1413ee1cd860c388eaa2599e70745
Ajith <ajithkumarhari011@gmail.com>
2023-07-20 11:58:45 +0530

    twenty sixth file update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine','ejs')
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.use('/',userRouter)
 app.use('/admin',adminRouter)
